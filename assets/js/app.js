e2198c60db21f30130dc28a07e64a21d6f8e9d4c
abdulrahman <konaalmges@gmail.com>
2026-02-20 02:13:19 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -124,4 +124,12 @@ function startTimer(){
     seconds--;
 
   },1000);
+  window.addEventListener("scroll",()=>{
+  const navbar = document.querySelector(".navbar");
+  if(window.scrollY > 50){
+    navbar.classList.add("scrolled");
+  }else{
+    navbar.classList.remove("scrolled");
+  }
+});
 }
